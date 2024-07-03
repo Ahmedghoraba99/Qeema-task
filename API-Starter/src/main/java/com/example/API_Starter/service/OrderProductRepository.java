dd25a9539d55180c15d28eaa223257946f47f1f8
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-07-03 19:07:23 +0300

    (Cleaning up the code base by deleting unused interfaces and repositories)

--- API-Starter/src/main/java/com/example/API_Starter/service/OrderProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.API_Starter.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.API_Starter.entity.OrderProduct;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
-
-}
