51dee9870fb2ab91861cf23a0864bd8ad1f164e4
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-07-03 23:18:49 +0300

    (feat) Refactored ApiStarterApplication for product and user service enhancements, updated imports and seeding data

--- API-Starter/src/main/java/com/example/API_Starter/ApiStarterApplication.java
+++ API-Starter/src/main/java/com/example/API_Starter/ApiStarterApplication.java
@@ -7,30 +7,36 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.API_Starter.entity.Product;
 import com.example.API_Starter.entity.Users;
-import com.example.API_Starter.service.ProductServiceImpl;
-import com.example.API_Starter.service.UsersServiceImpl;
+import com.example.API_Starter.service.ProductService;
+import com.example.API_Starter.service.UsersService;
 
 @SpringBootApplication
 public class ApiStarterApplication {
 
     @Autowired
-    private ProductServiceImpl productService;
+    private ProductService productService;
+
     @Autowired
-    private UsersServiceImpl userService;
+    private @Lazy
+    UsersService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiStarterApplication.class, args);
-
     }
 
-    // CLI 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
-        return runner -> {
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
             System.out.println("\n ********************* \n App started");
             System.out.println("Seeding data...");
 
@@ -40,19 +46,16 @@ public class ApiStarterApplication {
             products.add(new Product("Apple", 100, 9));
             products.add(new Product("Banana", 200, 8));
             products.add(new Product("Cherry", 300, 7));
-            products.forEach(product -> productService.add(product));
+            products.forEach(product -> productService.save(product));
 
             // Users seeds
             System.out.println("*****Users seeding*****");
             ArrayList<Users> users = new ArrayList<>();
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            String encodedPassword = passwordEncoder.encode("123456789");
+            String encodedPassword = passwordEncoder().encode("123456789");
             users.add(new Users("Ahmed", "ahmed@gmail.com", encodedPassword, "ADMIN"));
             users.add(new Users("Mohamed", "mohamed@gmail.com", encodedPassword, "USER"));
             users.add(new Users("Ali", "ali@gmail.com", encodedPassword, "USER"));
-            users.forEach(user -> userService.add(user));
-
+            users.forEach(user -> userService.save(user));
         };
     }
-
 }
