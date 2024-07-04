12b229112f77267be47e303b55a25ec5db024472
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-07-04 20:36:46 +0300

    (feat) Refactored ProductService for product service enhancements, added methods for product creation, product finding, product update and product conversion

--- API-Starter/src/main/java/com/example/API_Starter/service/ProductServiceImpl.java
+++ API-Starter/src/main/java/com/example/API_Starter/service/ProductServiceImpl.java
@@ -1,9 +1,17 @@
 package com.example.API_Starter.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.API_Starter.entity.Product;
 import com.example.API_Starter.repository.ProductRepository;
+import com.example.API_Starter.rest.controlleradvice.products.request.ProductRequest;
+import com.example.API_Starter.rest.controlleradvice.products.response.ProductResponse;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProductServiceImpl {
@@ -13,6 +21,38 @@ public class ProductServiceImpl {
     @Autowired
     ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public Product requestToProduct(ProductRequest productRequest) {
+        return new Product(
+                productRequest.getName(),
+                productRequest.getPrice(),
+                productRequest.getQuantity()
+        );
+    }
+
+    public ProductResponse productToResponse(Product product) {
+        return new ProductResponse(product);
+    }
+
+    public List<ProductResponse> productsToResponses(List<Product> products) {
+        return products.stream()
+                .map(this::productToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Product updateProduct(ProductRequest productRequest, String id) {
+        int productId = Integer.parseInt(id);
+        productRepository.findById(productId).ifPresent(
+                product -> {
+                    product.setName(productRequest.getName());
+                    product.setPrice(productRequest.getPrice());
+                    product.setQuantity(productRequest.getQuantity());
+                    productRepository.save(product);
+                }
+        );
+        return productRepository.findById(productId).get();
     }
 
 }
