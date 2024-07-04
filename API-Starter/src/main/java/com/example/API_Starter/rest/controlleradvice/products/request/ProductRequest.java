b591485c5c0937bdfcb9d93408e75d5e6d7a1e63
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-07-04 20:36:17 +0300

    (feat) Refactored ProductRequest and ProductResponse for product service enhancements, added constructors, getters and setters for product request and response

---
+++ API-Starter/src/main/java/com/example/API_Starter/rest/controlleradvice/products/request/ProductRequest.java
@@ -0,0 +1,32 @@
+package com.example.API_Starter.rest.controlleradvice.products.request;
+
+public class ProductRequest {
+
+    private int quantity;
+    private String name;
+    private double price;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
