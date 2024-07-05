2464667334962d6ef718acc45fd3bc9d7868a0a9
Ahmed Ghoraba <ahmed.12.2011@gmail.com>
2024-07-05 06:43:10 +0300

    (feat) Refactored UsersService for auth service enhancements, added registerUser method and registerationRequestToUser function, enhanced user registration

--- API-Starter/src/main/java/com/example/api/service/UsersService.java
+++ API-Starter/src/main/java/com/example/api/service/UsersService.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import com.example.api.entity.Users;
 import com.example.api.repository.UserRepository;
+import com.example.api.rest.controlleradvice.auth.requests.RegisterationRequest;
+import com.example.api.rest.controlleradvice.auth.response.RegisterationResponse;
 
 public interface UsersService extends UserRepository {
 
     Optional<Users> findByEmail(String email);
 
+    RegisterationResponse registerUser(RegisterationRequest registerationRequest);
 }
--- API-Starter/src/main/java/com/example/api/service/UsersServiceImpl.java
+++ API-Starter/src/main/java/com/example/api/service/UsersServiceImpl.java
@@ -13,6 +13,8 @@ import com.example.api.entity.UserPrincipal;
 import com.example.api.entity.Users;
 import com.example.api.repository.UserRepository;
 import com.example.api.rest.controlleradvice.UsersResponses.exceptions.UserNotFoundException;
+import com.example.api.rest.controlleradvice.auth.requests.RegisterationRequest;
+import com.example.api.rest.controlleradvice.auth.response.RegisterationResponse;
 
 @Service
 public class UsersServiceImpl implements UserDetailsService {
@@ -24,6 +26,20 @@ public class UsersServiceImpl implements UserDetailsService {
     public UsersServiceImpl(@Lazy UsersService usersService, UserRepository userRepository) {
         this.usersService = usersService;
         this.userRepository = userRepository;
+    }
+
+    private Users registerationRequestToUser(RegisterationRequest registerationRequest) {
+        return new Users(
+                registerationRequest.getName(),
+                registerationRequest.getEmail(),
+                registerationRequest.getPassword()
+        );
+    }
+
+    public RegisterationResponse registerUser(RegisterationRequest registerationRequest) {
+        Users user = registerationRequestToUser(registerationRequest);
+        userRepository.save(user);
+        return new RegisterationResponse(user, "User registered successfully");
     }
 
     public Optional<Users> findByEmail(String email) {
